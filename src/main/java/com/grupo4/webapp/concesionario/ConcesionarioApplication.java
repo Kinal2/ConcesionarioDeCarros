00d608e0f4fdc17ec71afb831821948cfddb1c89
Cristian Lima <cristiannlima2@gmail.com>
2024-08-14 08:55:20 -0600

    Initial commit

---
+++ src/main/java/com/grupo4/webapp/concesionario/ConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.webapp.concesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionarioApplication.class, args);
+	}
+
+}
